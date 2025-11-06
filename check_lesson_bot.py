56e08eecd679d006ba93a110dd44b17c5740035e
redkrayn <redkrayn@yandex.ru>
2025-11-07 00:21:18 +0800

    Update project
    - Обернул данные в main()
    - Убрал асинхронность везде кроме сообщений, чтобы все не переделывать, пользуюсь теперь python-telegram_bot
    - Поменял название переменной data
    - Убрал print у таймаута чтобы не флудил
    - Добавил задержку подключения, при разрыве соединения

--- check_lesson_bot.py
+++ check_lesson_bot.py
@@ -1,67 +1,71 @@
 import requests
-from aiogram import Bot, Dispatcher
 import asyncio
-
+import time
 from environs import Env
+from telegram import Bot
 
 
-env = Env()
-env.read_env()
-
-TELEGRAM_BOT_TOKEN = env('TELEGRAM_BOT_TOKEN')
-TELEGRAM_CHAT_ID = env('TELEGRAM_CHAT_ID')
-
-bot = Bot(token=TELEGRAM_BOT_TOKEN)
-dp = Dispatcher()
+def send_self_message(bot, chat_id, text: str):
+    asyncio.run(bot.send_message(chat_id=chat_id, text=text))
 
 
-async def send_self_message(text: str):
-    await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=text)
-
-
-async def check_update_lesson():
+def check_update_lesson(bot, chat_id, devman_token):
     url = 'https://dvmn.org/api/long_polling/'
-    devman_token = env('DEVMAN_TOKEN')
     headers = {
         'Authorization': f'Token {devman_token}'
     }
     timestamp = None
+
     while True:
         params = {}
         if timestamp is not None:
             params['timestamp'] = timestamp
-        try:
-            response = requests.get(url, headers=headers, params=params, timeout=86400)
-            response.raise_for_status()
-            data = response.json()
 
-            if data.get('status') == 'timeout':
-                timestamp = data.get('timestamp_to_request')
-            elif data.get('status') == 'found':
-                timestamp = data.get('last_attempt_timestamp', timestamp)
-                if data['new_attempts'][0].get('is_negative'):
-                    await send_self_message(
+        try:
+            response = requests.get(url, headers=headers, params=params, timeout=90)
+            response.raise_for_status()
+            devman_response = response.json()
+
+            if devman_response.get('status') == 'timeout':
+                timestamp = devman_response.get('timestamp_to_request')
+            elif devman_response.get('status') == 'found':
+                timestamp = devman_response.get('last_attempt_timestamp', timestamp)
+
+                if devman_response['new_attempts'][0].get('is_negative'):
+                    send_self_message(
+                        bot,
+                        chat_id,
                         'У вас проверили работу "Отправляем уведомления о проверке работ"\n'
                         'К сожалению, в работе нашлись ошибки.\n'
-                        f'Ссылка на урок: {data['new_attempts'][0].get('lesson_url')}'
+                        f'Ссылка на урок: {devman_response["new_attempts"][0].get("lesson_url")}'
                     )
-                elif not data['new_attempts'][0].get('is_negative'):
-                    await send_self_message(
+                else:
+                    send_self_message(
+                        bot,
+                        chat_id,
                         'У вас проверили работу "Отправляем уведомления о проверке работ"\n'
                         'Преподавателю всё понравилось, можно приступать к следующему уроку!'
                     )
 
         except requests.exceptions.ReadTimeout:
-            print('timeout')
             continue
         except requests.exceptions.ConnectionError:
-            print('no internet')
+            print('Проблемы с интернетом, пробуем переподключиться...')
+            time.sleep(5)
             continue
 
 
-async def main():
-    await check_update_lesson()
+def main():
+    env = Env()
+    env.read_env()
+
+    telegram_bot_token = env('TELEGRAM_BOT_TOKEN')
+    telegram_chat_id = env('TELEGRAM_CHAT_ID')
+    devman_token = env('DEVMAN_TOKEN')
+
+    bot = Bot(token=telegram_bot_token)
+    check_update_lesson(bot, telegram_chat_id, devman_token)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
